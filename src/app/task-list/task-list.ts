7285e9322cc11833afee229052977103200dfc0b
Pedro Rodrigues <pedrorodrigues1143x@gmail.com>
2025-12-16 20:07:44 -0300

    funcionalidades dos componentes da lista de tarefas

--- src/app/task-list/task-list.ts
+++ src/app/task-list/task-list.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
 import { TaskFilter } from "../task-filter/task-filter";
+import { TaskService } from '../services/task';
+import { Task } from '../models/task';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-task-list',
-  imports: [TaskFilter],
+  imports: [TaskFilter, CommonModule, FormsModule, RouterModule],
   templateUrl: './task-list.html',
   styleUrl: './task-list.css',
 })
 export class TaskList {
+
+  constructor(private taskService: TaskService) { }
+
+  tasks: Array<Task> = [];
+
+  newTask = new Task();
+
+  ngOnInit() {
+    this.tasks = this.taskService.getTasks();
+  }
+
+  addTask() {
+
+    this.taskService.addTask(this.newTask);
+
+    this.newTask = new Task();
+  }
+
+  removeTask(task: Task) {
+    
+    this.taskService.removeTask(task);
+  }
+
+  updateTasks() {
+
+    this.taskService.updateTasks();
+  }
+
+  filterTasks(filter: string) {
+
+    if (filter !== '') {
+      this.tasks = this.tasks.filter(c => c.name.includes(filter))
+    }
+    else {
+      this.tasks = this.taskService.getTasks();
+    }
+  }
 
 }
